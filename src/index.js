021527d35994bbfe72d798810c82b1f27b92069a
Umidullo Suyunov <suyunovumidullo@gmail.com>
2020-09-30 18:47:18 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (!array || !array.length) {
+        return 0;
+    } else {
+        let minValue = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (minValue > array[i]) {
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0;
+    } else {
+        let maks = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (maks < array[i]) {
+                maks = array[i];
+            }
+        }
+        return maks;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (!array || !array.length) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let average = sum / array.length;
+        return average;
+    }
+};
